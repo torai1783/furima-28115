8bb90848aa72feb93e5100329025613a4dfc78c5
torai1783 <intosub1988@gmail.com>
2020-11-24 12:34:22 +0900

    テストコード・バリテーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,11 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
   VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: VALID_PASSWORD_REGEX, message: 'Include both letters and numbers'
+  validates_format_of :password, with: VALID_PASSWORD_REGEX,  message: 'Include both letters and numbers', length: { minimum:6 }
+  validates :family_name_jp, presence: true
+  validates :first_name_jp, presence: true
+  KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
+  validates :family_name_kana, presence: true, format: { with: KATAKANA_REGEXP }
+  validates :first_name_kana, presence: true, format: { with: KATAKANA_REGEXP }
+  validates :birthday, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
    describe 'ユーザー新規登録' do
     it "nicknameが空だと登録できない" do
-      user = User.new(nickname: "", email: "kkk@gmail.com", password: "000000", password_confirmation: "000000")
+      user = FactoryBot.build(:user)
+      user.nickname = ""
       user.valid?
       expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
     it "emailが空だと登録できない" do
-      user = User.new(nickname:"yamada", email: "",password:"000000", password_confirmation: "000000" )
+      user = FactoryBot.build(:user)
+      user.email = ""
       user.valid?
       expect(user.errors.full_messages).to include("Email can't be blank")
     end
     it "パスワードが空だと登録できない" do
-      user = User.new(nickname:"yamada", email: "kkk@gmail.com",password:"", password_confirmation: "000000" )
+      user = FactoryBot.build(:user)
+      user.password = ""
       user.valid?
       expect(user.errors.full_messages).to include("Password can't be blank")
     end
     it "2回目のパスワードが空だと登録できない" do
-      user = User.new(nickname:"yamada", email: "kkk@gmail.com",password:"000000", password_confirmation: "" )
+      user = FactoryBot.build(:user)
+      user.password_confirmation = ""
       user.valid?
       expect(user.errors.full_messages).to include("Password Include both letters and numbers")
     end
+    it "苗字が空だと登録できない" do
+      user = FactoryBot.build(:user)
+      user.family_name_jp = ""
+      user.valid?
+      expect(user.errors.full_messages).to include("Family name jp can't be blank")
+    end
+    it "名前が空だと登録できない" do
+      user = FactoryBot.build(:user)
+      user.first_name_jp = ""
+      user.valid?
+      expect(user.errors.full_messages).to include("First name jp can't be blank")
+    end
+    it "フリガナ(苗字)が空だと登録できない" do
+      user = FactoryBot.build(:user)
+      user.family_name_kana = ""
+      user.valid?
+      expect(user.errors.full_messages).to include("Family name kana can't be blank")
+    end
+    it "フリガナ(名前)が空だと登録できない" do
+      user = FactoryBot.build(:user)
+      user.first_name_kana = ""
+      user.valid?
+      expect(user.errors.full_messages).to include("First name kana can't be blank")
+    end
+    it "生年月日が空だと登録できない" do
+      user = FactoryBot.build(:user)
+      user.birthday = ""
+      user.valid?  
+      expect(user.errors.full_messages).to include("Birthday can't be blank")
+    end
   end
 end
+
