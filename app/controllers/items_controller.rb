f55cb5df87c6c450ddf0d01859cda75ecf847b1d
torai1783 <intosub1988@gmail.com>
2020-11-25 16:08:38 +0900

    first commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
-#class ItemsController < ApplicationController
-  #def index
-   # @items = Item.all
-  #end
-#end
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+end
