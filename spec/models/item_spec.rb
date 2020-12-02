7d93674635edb3891b95b2b80f7229c887ac521f
torai1783 <intosub1988@gmail.com>
2020-12-02 13:11:23 +0900

    修正-5

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -70,15 +70,19 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price is not included in the list", "Price can't be blank", "Price is invalid")
       end
       it "価格半角数字で入力されないと出品できない" do
-        @item.price = '500'
+        @item.price = '５００'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not included in the list", "Price can't be blank", "Price is invalid")
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
       end
       it "価格は300~9,999,999の間で設定しないと出品できない" do
         @item.price = 299
-        @item.price = 100000000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not included in the list", "Price can't be blank", "Price is invalid")
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
+      end
+      it "価格は300~9,999,999の間で設定しないと出品できない" do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
       end
     end
   end
