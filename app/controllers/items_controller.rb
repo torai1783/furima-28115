ec728df6776c461a53288d7f4e0daa093c92f492
torai1783 <intosub1988@gmail.com>
2020-12-07 12:40:33 +0900

    編集ボタンから詳細ページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,6 +15,11 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     @item.update(item_params)
+    if @item.save
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   #def destory
