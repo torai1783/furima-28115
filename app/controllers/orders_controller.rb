2f023f3facb7580cdd58de695d318ca8b272f777
torai1783 <intosub1988@gmail.com>
2020-12-17 16:58:47 +0900

    購[修正]入済み品はURL入力後、topへ遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,13 +5,16 @@ class OrdersController < ApplicationController
   def index
     @order = OrderItem.new
     @item = Item.find(params[:item_id])
-    # 出品者はURLを直接入力して購入ページに遷移しようとすると、トップページに遷移すること
-    if @item.user_id == current_user.id
+     #出品者はURLを直接入力して購入ページに遷移しようとすると、トップページに遷移すること
+     #URLを直接入力して購入済み商品の購入ページへ遷移しようとすると、トップページに遷移すること
+    if @item.user_id == current_user.id 
        redirect_to root_path
     end
-    if @order
+
+    if @item.users_item.present?
        redirect_to root_path
     end
+
   end
 
   def create
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  has_one :users
-  has_one :items
+  has_one :user
+  has_one :item
   belongs_to :users_item
 
 end
